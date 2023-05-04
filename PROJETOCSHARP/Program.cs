7e4656099cf2445b89afbe33d4a0664e75feec1e
vitor marinho <itachimarinho@gmail.com>
2023-05-04 11:33:43 -0300

    EX PATH

--- PROJETOCSHARP/Program.cs
+++ PROJETOCSHARP/Program.cs
@@ -115,7 +115,7 @@ namespace PROJETOCSHARP
                 {"64", ExemploFileInfo.Executar },
                 {"65", diretorios.Executar },
                 {"66", ExemploDirectoryInfo.Executar },
-
+                {"67", ExemploPath.Executar },
 
 
             }); ; ; ;
@@ -124,7 +124,7 @@ namespace PROJETOCSHARP
 
 
 
-            central.Executar("66");
+            central.Executar("67");
                 
                 
                 
